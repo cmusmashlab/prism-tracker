22de2812d2145a37d7354c5cba80d4cc4ec19863
rikky0611 <a_ra_ra_rikky0611@yahoo.co.jp>
2023-03-13 18:49:58 -0400

    Remove unused code

--- analysis/src/prism_tracker/preprocessing/params.py
+++ analysis/src/prism_tracker/preprocessing/params.py
@@ -7,16 +7,11 @@ STFT_HOP_LENGTH_SECONDS = 0.03
 EXAMPLE_WINDOW_SECONDS = 96 * 0.03   # Each example contains 96 10ms frames
 # EXAMPLE_HOP_SECONDS = 96*0.03*0.1    # with zero overlap.
 EXAMPLE_HOP_SECONDS = 0.2
-# Frames in input mel-spectrogram patch.
-NUM_FRAMES = 1 + int(EXAMPLE_WINDOW_SECONDS / STFT_HOP_LENGTH_SECONDS)
 
 MEL_MIN_HZ = 125
 MEL_MAX_HZ = 7500
 LOG_OFFSET = 0.001  # Offset used for stabilized log of input mel-spectrogram.
-
-NUM_BANDS = 64  # Frequency bands in input mel-spectrogram patch.
-NUM_MEL_BINS = NUM_BANDS
-
+NUM_MEL_BINS = 64  # Frequency bands in input mel-spectrogram patch.
 
 """
 Motion
